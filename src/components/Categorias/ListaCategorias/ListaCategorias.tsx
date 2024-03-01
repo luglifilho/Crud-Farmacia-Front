936b1001eec336c7029a82dc853759d30e1b84c8
Daniel <luglifilho@hotmail.com>
2024-03-01 09:03:51 -0300

    ajuste no component Lista

--- src/components/Categorias/ListaCategorias/ListaCategorias.tsx
+++ src/components/Categorias/ListaCategorias/ListaCategorias.tsx
@@ -18,8 +18,7 @@ function ListaCategorias() {
 
   async function buscarCategorias() {
     try {
-      await buscar('/categoria', setCategorias, {
-      });
+      await buscar('/categoria', setCategorias,);
     } catch (error: any) {
       if (error.toString().includes('403')) {
         alert('O token expirou, favor logar novamente')    
